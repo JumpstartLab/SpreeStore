b2ec07e686d3b4f5b70eb376c76de29be484f4d3
Franklin Webber <franklin.webber@gmail.com>
2013-08-02 14:56:14 -0600

    Initial Spree Store

---
+++ README.md
@@ -0,0 +1,19 @@
+# Spree
+
+The following is an up and running Spree Store after completing the [getting
+started tutorial](http://guides.spreecommerce.com/developer/getting_started_tutorial.html).
+
+```
+$ bundle install
+$ rake db:migrate
+$ rails s
+```
+
+## User Accounts
+
+Admin: spree@example.com
+Password: spree123
+
+## Admin Section
+
+Visit [http://localhost:3000/admin](http://localhost:3000/admin).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jslstore::Application.config.secret_token = '1f924ef270f4250a5c5c4d60a3e6de3b976d0ffddc80d8f185187d69dd147cecbe7859c99af688999c54853e32eb45a0f89a2fe87bc3c8f19a120b72b9ed7e97'
